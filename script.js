fdf7967c8337322a656312ee2b1445fd54d89ccb
Razia Munir <irazia.munir@gmail.com>
2023-07-06 08:27:07 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let timerDisplay = document.querySelector(timerDisplay);
+let start = document.getElementById(startbtn);
+let stop = document.getElementById(stopbtn);
+let Reset = document.getElementById(resetbtn);
+let msec =00;
+let sec =00;
+let min =00;
