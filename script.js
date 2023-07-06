fcded395173e640d1b49dbf4121039327e6b24b6
Razia Munir <irazia.munir@gmail.com>
2023-07-06 11:18:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let timerDisplay = document.querySelector('.timerDisplay');
-let start = document.getElementById('startbtn');
-let stop = document.getElementById('stopbtn');
-let Reset = document.getElementById('resetbtn');
+let start = document.getElementById('startBtn');
+let stop = document.getElementById('stopBtn');
+let Reset = document.getElementById('resetBtn');
 
 let msec =00;
 let secs =00;
@@ -9,21 +9,21 @@ let mins =00;
 
 let timerId = null; 
 
-startbtn.addEventListener('click', function(){
+startBtn.addEventListener('click', function(){
     if(timerId !== null){
         clearInterval(timerId);
     }
     timerId = setInterval(startTimer,10);
 });
 
-stopbtn.addEventListener('click', function(){
+stopBtn.addEventListener('click', function(){
     clearInterval(timerId);
 });
 
-resetbtn.addEventListener('click', function(){
+resetBtn.addEventListener('click', function(){
     clearInterval(timerId);
     timerDisplay.innerHTML = `00 : 00 : 00`;
-    msec=secs=mi=0;
+    msec=secs=mins=0;
 });
 
 function startTimer() {
